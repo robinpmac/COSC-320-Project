264e7341482c9fcef1cba0c3bf446b40e78877f9
robinpmac <robinpmac@gmail.com>
2020-04-07 14:06:54 -0700

    Update FlightSearch.java
    Added loadAll method and started on main

--- src/main/FlightSearch.java
+++ src/main/FlightSearch.java
@@ -1,10 +1,44 @@
 package main;
 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
 public class FlightSearch {
+	
+	private static ArrayList<Airport> airports;
+	private static ArrayList<Flight> flights;
+	
+	
+	private static int numAirports;
 
+	//Pre-processing for the algorithm
+	public static void loadAll() {
+		airports = CSVReader.loadAirports();
+		flights = CSVReader.loadFlights();
+		
+		numAirports = airports.size();
+		
+		//Load flights into airports
+		for(int i=0; i<airports.size(); i++) {
+			airports.get(i).loadFlightData(flights);
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		loadAll();
+		
+		ArrayList<Airport> unvisited = new ArrayList<Airport>(numAirports);
+		
+		for(int i=0; i<airports.size(); i++) {
+			unvisited.add(airports.get(i));
+		}
+		
 
+	}
+	
+	int minDistance() {
+		return -1;
 	}
 
 }
