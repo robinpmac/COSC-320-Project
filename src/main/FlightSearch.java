6cefcaa4beaa23f4cb3e6e80d0e1d1bb7556e478
robinpmac <robinpmac@gmail.com>
2020-04-07 14:37:55 -0700

    Update FlightSearch.java
    Added method to set depLoc and arrLoc from given Airport code

--- src/main/FlightSearch.java
+++ src/main/FlightSearch.java
@@ -9,6 +9,11 @@ public class FlightSearch {
 	private static ArrayList<Airport> airports;
 	private static ArrayList<Flight> flights;
 	
+	private static String depLocCode = "YVR";
+	private static String arrLocCode = "YYZ";
+	
+	private static Airport depLoc = null;
+	private static Airport arrLoc = null;
 	
 	private static int numAirports;
 
@@ -25,6 +30,39 @@ public class FlightSearch {
 		}
 	}
 	
+	public static boolean setStartFinish() {
+		boolean loadSuccess = false;
+		boolean depLocLoad = false;
+		boolean arrLocLoad = false;
+		Airport temp = null;
+		
+		Iterator<Airport> itr = airports.iterator();
+		
+		while(itr.hasNext() && loadSuccess == false) {
+			temp = itr.next();
+			
+			if(temp.getCode().equals(depLocCode)) {
+				depLoc = temp;
+				depLocLoad = true;
+			}
+			
+			if(temp.getCode().equals(arrLocCode)) {
+				arrLoc = temp;
+				arrLocLoad = true;
+			}
+			
+			if(depLocLoad && arrLocLoad)
+				loadSuccess = true;	
+		}
+		
+		if(loadSuccess == false) {
+			depLoc = null;
+			arrLoc = null;
+		}
+		
+		return loadSuccess;
+	}
+	
 	public static void main(String[] args) {
 		loadAll();
 		
@@ -38,6 +76,11 @@ public class FlightSearch {
 	}
 	
 	int minDistance() {
+		int min = Integer.MAX_VALUE;
+		int minIdx = -1;
+		
+		
+		
 		return -1;
 	}
 
